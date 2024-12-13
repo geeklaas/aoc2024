3b0e872d0ffc31c9f20a16e2251dbada962520b3
Oliver Campeau <campeau.oliver@gmail.com>
2024-12-13 16:55:04 +0100

    Day 1 complete!

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,3 +1,5 @@
+# Problem 1
+
 listA = []
 listB = []
 
@@ -14,11 +16,31 @@ while len(content) > 0:
 listA.sort()
 listB.sort()
 
-#print(listA)
-#print(listB)
-
 i = 0
 while len(listA) > 0:
    i = i + abs(listA.pop(0) - listB.pop(0))
 else:
-    print(i)
+   print(i)
+
+
+# Problem 2
+
+listC = []
+listD = []
+
+file = open("D:/Repos/aoc2024/Day1/input.txt", "r")
+content = file.readlines()
+file.close()
+
+while len(content) > 0:
+   tempRow = (content.pop(0))
+   tempList = tempRow.split()
+   listC.append(int(tempList.pop(0)))
+   listD.append(int(tempList.pop(0)))  
+
+sim = 0
+while len(listC) > 0:
+   temp = listC.pop(0)
+   sim = sim + (temp*listD.count(temp))
+else:
+   print(sim)
